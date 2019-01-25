7b4fe1dcd80594757be1500da074a2807e53a8b7
technollama <47030087+technollama@users.noreply.github.com>
2019-01-25 12:21:41 +0000

    Update content_script.js

--- Source/content_script.js
+++ Source/content_script.js
@@ -36,10 +36,10 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
+	v = v.replace(/\bSmart City\b/g, "Panopticon");
+	v = v.replace(/\bSmart city\b/g, "Panopticon");
+	v = v.replace(/\bsmart City\b/g, "Panopticon");
+	v = v.replace(/\bsmart city\b/g, "panopticon");
 	
 	textNode.nodeValue = v;
 }
